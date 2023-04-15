c9d3b439579c3135b2f1a25d42da70ce5fa6c646
Soliev <thrsoliev@gmail.com>
2023-04-15 12:32:25 +0500

    Add all file

---
+++ scripts/app.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
